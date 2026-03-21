f87e6cf40da4c6675e0431622b3a9232419a5099
Sinnez1 <99955247+Sinnez1@users.noreply.github.com>
2026-03-22 00:00:54 +0800

    Fix checkstyle warnings

--- README.md
+++ README.md
@@ -7,12 +7,12 @@
   * Adding contacts for students in your class.
   * Tracking students' attendance for classes.
   * Tracking students' participation in classes.
-  * Tracking students' submission for assignments. 
+  * Tracking students' submission for assignments.
 
   🏗 Design & Architecture
   * It is designed using **object-oriented principles** with **clean architecture** and **modular structure**.
   * It includes comprehensive **user and developer documentation** to support both usage and future development.
-  
+
   📖 Documentation
   * This project is based on the AddressBook-Level3 project created by the [SE-EDU initiative](https://se-education.org).
   * For the detailed documentation of this project, see the **[Teacher's Assistant's Assistant Product Website](https://github.com/AY2526S2-CS2103T-F14-1/tp)**.
--- src/main/java/seedu/address/model/person/Email.java
+++ src/main/java/seedu/address/model/person/Email.java
@@ -8,7 +8,7 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  * Guarantees: immutable; is valid as declared in {@link #isValidEmail(String)}
  */
 public class Email {
-
+    public static final String INVALID_EMAIL_FORMAT = "Invalid email format.";
     private static final String SPECIAL_CHARACTERS = "+_.-";
     public static final String MESSAGE_CONSTRAINTS = "Emails should be of the format local-part@domain "
             + "and adhere to the following constraints:\n"
@@ -21,7 +21,7 @@ public class Email {
             + "    - end with a domain label at least 2 characters long\n"
             + "    - have each domain label start and end with alphanumeric characters\n"
             + "    - have each domain label consist of alphanumeric characters, separated only by hyphens, if any.";
-    public static final String INVALID_EMAIL_FORMAT = "Invalid email format.";
+
     // alphanumeric and special characters
     private static final String ALPHANUMERIC_NO_UNDERSCORE = "[^\\W_]+"; // alphanumeric characters except underscore
     private static final String LOCAL_PART_REGEX = "^" + ALPHANUMERIC_NO_UNDERSCORE + "([" + SPECIAL_CHARACTERS + "]"

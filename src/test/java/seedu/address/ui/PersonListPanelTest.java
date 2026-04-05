01cec1f3a7af8b2531ca377ac3a9e310aefbff92
Kawai Cheng <cheng.kw0123@gmail.com>
2026-04-05 18:44:46 +0800

    Remove unused import assertTrue

--- src/test/java/seedu/address/ui/PersonListPanelTest.java
+++ src/test/java/seedu/address/ui/PersonListPanelTest.java
@@ -2,7 +2,6 @@ package seedu.address.ui;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDate;
 import java.util.List;
